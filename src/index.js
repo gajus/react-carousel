9ab193a01f55e193a9834da3fc34c5438b615f9c
Charanjit Singh <ckhabra007@gmail.com>
2015-11-22 10:56:31 +0530

    Implement pragmatist
    - Fix linting errors

--- src/index.js
+++ src/index.js
@@ -9,26 +9,18 @@ import ReactDOM from 'react-dom';
 import CSSModules from 'react-css-modules';
 import styles from './carousel.scss';
 
+/* eslint-disable react/no-set-state, react/no-did-mount-set-state */
 class Carousel extends Component {
-    constructor (props) {
-        super(props);
-
-        this.state = {maxWidth: null};
-        this.resizeEventListener = _.debounce(() => {
-            this.setState({maxWidth: ReactDOM.findDOMNode(this.refs.wrapper).offsetWidth});
-        }, 100);
-    }
-
     static propTypes = {
         activeItemId: PropTypes.string,
-        items: PropTypes.arrayOf(PropTypes.object).isRequired,
-        firstVisibleIndex: PropTypes.number,
-        itemWidth: PropTypes.number,
-        scrollStepDistance: PropTypes.number,
-        itemMargin: PropTypes.number,
         controlWidth: PropTypes.number,
+        firstVisibleIndex: PropTypes.number,
+        itemMargin: PropTypes.number,
+        itemWidth: PropTypes.number,
+        items: PropTypes.arrayOf(PropTypes.object).isRequired,
         onItemActivate: PropTypes.func,
-        onItemsScroll: PropTypes.func
+        onItemsScroll: PropTypes.func,
+        scrollStepDistance: PropTypes.number
     }
 
     static defaultProps = {
@@ -38,6 +30,15 @@ class Carousel extends Component {
         controlWidth: 30,
         itemMargin: 1
     };
+
+    constructor (props) {
+        super(props);
+
+        this.state = {maxWidth: null};
+        this.resizeEventListener = _.debounce(() => {
+            this.setState({maxWidth: ReactDOM.findDOMNode(this.refs.wrapper).offsetWidth});
+        }, 100);
+    }
 
     componentDidMount () {
         this.setState({maxWidth: ReactDOM.findDOMNode(this.refs.wrapper).offsetWidth});
@@ -52,15 +53,15 @@ class Carousel extends Component {
      * Get the index component should (safely) scroll to
      *
      * @param {Object} options
-     * @property {Number} options.totalItems Total number of items being displayed
-     * @property {Number} options.currentVisibleIndex Index of item in front
-     * @property {String} options.direction next|previous Direction in which to scroll
-     * @property {Number} options.visibleItemsCount Number of items displayed at one time in component
-     * @property {Number} options.stepDistance Number of items to scroll in one step
+     * @property {number} options.totalItems Total number of items being displayed
+     * @property {number} options.currentVisibleIndex Index of item in front
+     * @property {string} options.direction next|previous Direction in which to scroll
+     * @property {number} options.visibleItemsCount Number of items displayed at one time in component
+     * @property {number} options.stepDistance Number of items to scroll in one step
      *
-     * @returns {Number} Index of item to which to scroll
+     * @returns {number} Index of item to which to scroll
      */
-    getIndexToScrollTo ({totalItems, firstVisibleIndex, direction, visibleItemsCount, scrollStepDistance}) {
+    getIndexToScrollTo = ({totalItems, firstVisibleIndex, direction, visibleItemsCount, scrollStepDistance}) => {
         let index,
             itemsBehindVisible,
             itemsBeyondVisible;
@@ -85,13 +86,12 @@ class Carousel extends Component {
      * @param {number} visibleItemsCount Number Number of items visible at the moment
      * @returns {boolean}
      */
-    isNextButtonVisible (firstVisibleIndex, totalItems, visibleItemsCount) {
+    isNextButtonVisible = (firstVisibleIndex, totalItems, visibleItemsCount) => {
         return firstVisibleIndex < totalItems - visibleItemsCount;
     }
 
     /**
-     * Tells if the previous control button shall be visible or not
-     *
+     * Tells if the previous control button shall be visible or not.
      * @param {number} firstVisibleIndex Index of the item to which the carousel is scrolled to
      * @returns {boolean}
      */
@@ -100,7 +100,7 @@ class Carousel extends Component {
     }
 
     /**
-     * Get number of items that can be shown at a point in time
+     * Get number of items that can be shown at a point in time.
      *
      * @param {Object} options Arguments in an object because they are too many
      * @property {number} firstVisibleIndex Index to which the carousel is scrolled to i.e first visible index
@@ -111,7 +111,7 @@ class Carousel extends Component {
      * @property {number} itemMargin Margin between items
      * @returns {number}
      */
-    getVisibleItemsCount ({firstVisibleIndex, maxWidth, totalItems, itemWidth, controlWidth, itemMargin}) {
+    getVisibleItemsCount = ({firstVisibleIndex, maxWidth, totalItems, itemWidth, controlWidth, itemMargin}) => {
         let availableWidth,
             calculateVisibleItemsCount,
             nextButtonVisible,
@@ -119,7 +119,7 @@ class Carousel extends Component {
             visibleItemsCount;
 
         /**
-         * Get the number of visible items which can be shown at a time
+         * Get the number of visible items which can be shown at a time.
          *
          * @param {number} fullWidth Total available width in which items are to be shown
          * @param {number} elementWidth Width of each individual item to be inserted
@@ -151,32 +151,96 @@ class Carousel extends Component {
         return visibleItemsCount;
     }
 
+    /**
+     * Helper function to avoid typing so many variables twice for two control buttons since most of the
+     * variables are already in scope
+     *
+     * @param {string} direction direction in which to scroll
+     * @param {number} visibleItemsCount Total number of visible items
+     * @returns {undefined}
+     */
+    handleScrollToDirection = (direction, visibleItemsCount) => {
+        let index;
+
+        index = this.getIndexToScrollTo({
+            direction,
+            totalItems: this.props.items.length,
+            firstVisibleIndex: this.props.firstVisibleIndex,
+            visibleItemsCount,
+            scrollStepDistance: this.props.firstVisibleIndex
+        });
+
+        this.props.onItemsScroll(index);
+    }
+
+    handleActivateItem = (item) => {
+        this.props.onItemActivate(item.key);
+    }
+
+    /**
+     * Creates functoin that creates JSX for one item
+     *
+     * @param {number} visibleItemsCount Total number of visible items
+     *
+     * @returns {Function}
+     */
+    getItemJsx = (visibleItemsCount) => {
+            /**
+             * Creates JSX for one item
+             *
+             * @param {JSX} item Valid JSX for the item's content
+             * @param {number} index Index of item in the items array
+             * @param {number} visibleItemsCount Total number of visible items
+             *
+             * @returns {JSX}
+             */
+        return (item, index) => {
+            let isVisible,
+                visibleItemIndeces;
+
+            visibleItemIndeces = _.range(this.props.firstVisibleIndex, visibleItemsCount + this.props.firstVisibleIndex);
+
+            isVisible = _.contains(visibleItemIndeces, index);
+
+            /*
+             * Here we could return only those items which need be visible, but I am returning all.
+             * Since React does optimal DOM operations, rendering all items at once and then updating their
+             * style is much less expensive then rendering a set of new list-items everytime.
+             */
+            return <li
+                key={item.key}
+                onClick={this.handleActivateItem}
+                style={{
+                    width: this.props.itemWidth,
+                    marginRight: this.props.itemMargin,
+                    display: isVisible ? 'list-item' : 'none'
+                }}
+                styleName={'cell' + (this.props.activeItemId === item.key ? '-active' : '')}
+                   >
+            {item}
+            </li>;
+        };
+    }
+
     render () {
         // declaring so many variables instead of directly using props to stay consistent with the coding
         // style adopted for other components
-        let activeItemId,
-            controlWidth,
+        let controlWidth,
             firstVisibleIndex,
-            getItemJsx,
             itemMargin,
             itemWidth,
             items,
             maxWidth,
             nextButtonVisible,
             prevButtonVisible,
-            scrollStepDistance,
-            scrollTo,
             totalItems,
-            visibleItemIndeces,
             visibleItemsCount;
 
         maxWidth = this.state.maxWidth;
         items = this.props.items;
         controlWidth = this.props.controlWidth;
-        activeItemId = this.props.activeItemId;
         firstVisibleIndex = this.props.firstVisibleIndex;
         itemWidth = this.props.itemWidth;
-        scrollStepDistance = this.props.scrollStepDistance;
         itemMargin = this.props.itemMargin;
         totalItems = items.length;
         prevButtonVisible = this.isPrevButtonVisible(firstVisibleIndex);
@@ -189,81 +253,31 @@ class Carousel extends Component {
             itemMargin
         });
         nextButtonVisible = this.isNextButtonVisible(firstVisibleIndex, totalItems, visibleItemsCount);
-        visibleItemIndeces = _.range(firstVisibleIndex, visibleItemsCount + firstVisibleIndex);
 
-        /**
-         * Helper function to avoid typing so many variables twice for two control buttons since most of the variables are already in scope
-         *
-         * @param {string} direction direction in which to scroll
-         * @returns {undefined}
-         */
-        scrollTo = (direction) => {
-            let index;
-
-            index = this.getIndexToScrollTo({
-                direction,
-                totalItems,
-                firstVisibleIndex,
-                visibleItemsCount,
-                scrollStepDistance
-            });
-
-            this.props.onItemsScroll(index);
-        };
-
-        /**
-         * Creates JSX for one item
-         *
-         * @param {JSX} item Valid JSX for the item's content
-         * @param {number} index Index of item in the items array
-         *
-         * @returns {JSX}
-         */
-        getItemJsx = (item, index) => {
-            let isVisible;
-
-            isVisible = _.contains(visibleItemIndeces, index);
-
-            /*
-             * Here we could return only those items which need be visible, but I am returning all.
-             * Since React does optimal DOM operations, rendering all items at once and then updating their
-             * style is much less expensive then rendering a set of new list-items everytime.
-             */
-            return <li
-                       styleName={`cell${activeItemId === item.key ? '-active' : ''}`}
-                       style={{
-                           width: itemWidth,
-                           marginRight: itemMargin,
-                           display: isVisible ? 'list-item' : 'none'
-                       }}
-                       key={item.key}
-                       onClick={() => this.props.onItemActivate(item.key)}>
-                       {item}
-            </li>;
-        };
-
-        return <div styleName="wrapper" ref="wrapper">
+        return <div ref='wrapper' styleName='wrapper'>
                 <ul styleName='carousel'>
 
-                    <li styleName="control-cell-previous"
+                    <li onClick={this.handleScrollToDirection('previous', visibleItemsCount)}
                         style={{
                             width: controlWidth,
                             marginRight: itemMargin,
                             display: prevButtonVisible ? 'list-item' : 'none'
                         }}
-                        onClick={() => scrollTo('previous')}>
-                        <span styleName="control-icon-previous"></span>
+                        styleName='control-cell-previous'
+                    >
+                        <span styleName='control-icon-previous'></span>
                     </li>
 
-                    {items.map(getItemJsx)}
+                    {items.map(this.getItemJsx(visibleItemsCount))}
 
-                    <li styleName="control-cell-next"
+                    <li onClick={this.handleScrollToDirection('next', visibleItemsCount)}
                         style={{
                             width: controlWidth,
                             display: nextButtonVisible ? 'list-item' : 'none'
                         }}
-                        onClick={() => scrollTo('next')} >
-                        <span styleName="control-icon-next"></span>
+                        styleName='control-cell-next'
+                    >
+                        <span styleName='control-icon-next'></span>
                     </li>
 
                 </ul>
